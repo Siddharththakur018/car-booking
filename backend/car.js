7393477ccfb531ff88a63dd66119a76cf78fb9dd
Siddharththakur018 <u803760@gmail.com>
2025-03-01 10:03:31 +0530

    Car.js

--- backend/car.js
+++ backend/car.js
@@ -7,7 +7,11 @@ const app = express();
 app.use(express.json());
 
 app.use("/cars", carRoute); 
-app.use(cors());
+app.use(cors({
+  origin: "*", // Allow requests from any origin (change this for security)
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed request methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+}));
 
 
 mongoose.connect(process.env.MONGO_URI_1 || "mongodb+srv://thakursiddharth530:Siddharth@car.rfyr9.mongodb.net/?retryWrites=true&w=majority&appName=Car")
